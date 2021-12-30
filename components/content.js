e0460e732712537a5975c9db4f92dbd71880416e
Graham <graham.trogdon@gmail.com>
2021-12-30 17:23:56 -0500

    pulls CSVs using gviz API

--- components/content.js
+++ components/content.js
@@ -3,7 +3,7 @@ import { Base } from "./base";
 import db from "../db";
 import { Data } from "../data";
 import XLSX from "xlsx";
-import sheetrock from "sheetrock";
+import { strFromU8 } from "fflate";
 
 /** @param {File} file */
 async function readLocalSheet(file) {
@@ -54,33 +54,23 @@ export class Content extends Base {
         onkeydown=${async (/** @type {InputEvent} e */ e) => {
           const target = /** @type {HTMLInputElement} */ (e.target);
           if (target.checkValidity() && e.key == "Enter") {
-            sheetrock({
-              url: e.target.value,
-              target: document.createElement("div"),
-              callback: (
-                /** @type {object} error */ error,
-                /** @type {object} options */ options,
-                /** @type {object} response */ response
-              ) => {
-                const result = response.rows.reduce((dataArr, rowObj, i) => {
-                  if (i) {
-                    let datum = {};
-                    for (let j = 0; j < rowObj.labels.length; j++) {
-                      datum[rowObj.labels[j].toLowerCase()] =
-                        rowObj.cellsArray[j];
-                      if (!("tags" in datum)) datum["tags"] = [];
-                    }
-                    dataArr.push(datum);
-                  }
-                  return dataArr;
-                }, []);
+            const urlEnd = target.value.indexOf("/edit")
+            if(urlEnd != -1) {
+              const sheetURL = `${target.value.slice(0, urlEnd)}/gviz/tq?tqx=out:csv&tq=SELECT *`; 
 
-                this.context.data = new Data(result);
-                document.querySelector("#loadStatus").innerHTML = `Loaded 
-                  ${this.context.data.allrows.length} rows with ${this.context.data.allFields.length} columns`;
-                this.context.state.update();
-              },
-            });
+              const result = await readLocalSheet(
+                await fetch(sheetURL).then((response) => response)
+              );
+
+              document.querySelector(
+                "#loadStatus"
+              ).innerHTML = `Loaded ${result.rows} rows with ${result.fields.length} columns`;
+
+              this.context.data = new Data(result.dataArray);
+              this.context.state.update();
+            } else {
+              console.error("Invalid Google sheet url");
+            } 
           }
         }}
       />
@@ -96,7 +86,6 @@ export class Content extends Base {
             "#loadStatus"
           ).innerHTML = `Loaded ${result.rows} rows with ${result.fields.length} columns`;
           this.context.data = new Data(result.dataArray);
-          console.log(this.context.data);
           this.context.state.update();
         }}
       />
